7683aa24a72ff5d8c541a70b323aac97a2a1e5be
Israel Martinez <marcemc@yahoo.com>
2019-04-18 19:23:08 -0400

    Add menu component

---
+++ src/components/Menu.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import MenuItem from './MenuItem'
+import MenuDisplay from './MenuDisplay'
+
+export default class Menu extends Component {
+  render(){
+    return(
+      <MenuItem />
+      <MenuDisplay />
+    )
+  }
+}
