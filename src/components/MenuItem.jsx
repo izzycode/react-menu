ededd04ecd4ac5e225930d4231c67c507c898ccd
Katherine Cordova <katc724@gmail.com>
2019-04-18 20:37:26 -0400

    added styles to MenuItem

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,9 +1,10 @@
 import React, {Component} from 'react'
+import css from './MenuItem.css'
 
 
 export default class MenuItem extends Component {
   state = {
-    name: 'southwestern sammy',
+    name: 'Southwest Sammy',
     image: 'https://img1.cookinglight.timeinc.net/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/2017/05/main/egg-in-nest-blt-sandwiches-1707p38.jpg?itok=-2zHWRHS',
     price: 1000
   }
@@ -18,9 +19,9 @@ export default class MenuItem extends Component {
 
   render(){
     return(
-      <div>
+      <div class= 'sandwich'>
         <h1>{this.state.name}</h1>
-        <img src={this.state.image} alt="sandwich" height='200px' />
+        <img className="sammy" src={this.state.image} alt="sandwich" height='300px' />
         <h2>{this.currency()}</h2>
       </div>
     )
